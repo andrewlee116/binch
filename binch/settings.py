b4ab017e58b0dd0c23a92ecba17221290cae5114
sylap <sylapaliyev@gmail.com>
2017-05-13 21:13:50 +0900

    Add static file support

--- binch/settings.py
+++ binch/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qon-%gw!%w)ca@=+&y5@djf$1d7pl%=$r28eoylexfz%)h0ps8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
