326cb740605178b8940c1474fb6ccc8a70939ec1
Andrew <leeandrew116@gmail.com>
2017-05-13 23:42:40 +0900

    few edits

--- binchgame/static/person.js
+++ binchgame/static/person.js
@@ -18,13 +18,14 @@ function Person(username) {
     this.x = this.x + this.xspeed * scl;
     this.y = this.y + this.yspeed * scl;
 
-    this.x = constrain(this.x, 0, width - scl);
-    this.y = constrain(this.y, 0, height - scl);
+    this.x = constrain(this.x, 6, width - scl-6);
+    this.y = constrain(this.y, 6, height - scl-6);
   }
 
   this.show = function() {
     fill(255);
     ellipse(this.x, this.y, scl*4, scl*4);
+
     fill(150);
     textSize(20);
     wid = textWidth(this.username);
