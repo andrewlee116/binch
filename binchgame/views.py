699e1c89218626d1249a9aa6bc061fa3c09666fa
Andrew <leeandrew116@gmail.com>
2017-05-13 20:42:50 +0900

    added more stuff

--- binchgame/views.py
+++ binchgame/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 
-# Create your views here.
+def index(request):
+    return render(request, 'binchgame/index.html',{})
